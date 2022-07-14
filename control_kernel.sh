e53e71e9db5141e144b382f261ed313981ab5024
RhysHuo <RhysHuo_1120272267@163.com>
2022-07-14 18:50:08 +0100

    Create control_kernel.sh

---
+++ control_kernel.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/RhysHuo/Fused_Vitis_Version.git
+cd Fused_Vitis_Version
+cp kernelMatrixmult_sw.cpp ..
+cd ..
+rm -rf Fused_Vitis_Version
+v++ -t sw_emu --platform xilinx_u250_gen3x16_xdma_4_1_202210_1 -c -k kernelmult1_sw -o'kernelmult1.sw_emu.xo' kernelMatrixmult_sw.cpp kernelMatrixmult.h xcl2.hpp
+v++ -t sw_emu --platform xilinx_u250_gen3x16_xdma_4_1_202210_1 --link kernelmult1.sw_emu.xo -o'kernelmult1.sw_emu.xclbin'
