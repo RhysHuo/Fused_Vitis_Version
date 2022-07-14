fead46b80a374bd18a2f62629bf9acded8c89e4b
RhysHuo <RhysHuo_1120272267@163.com>
2022-07-14 17:11:31 +0100

    Create host.h

---
+++ host.h
@@ -0,0 +1,16 @@
+#ifndef HOST_H_
+#define HOST_H_
+
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string>
+#include "ap_int.h"
+
+#define DTYPE_LENGTH 32
+typedef ap_uint<DTYPE_LENGTH> DTYPE;
+typedef unsigned int u32;
+
+
+#endif
