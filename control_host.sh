2549aaa256112779b51c957032e1fbdf2a320c1e
RhysHuo <RhysHuo_1120272267@163.com>
2022-07-15 16:21:30 +0100

    Update control_host.sh

--- control_host.sh
+++ control_host.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/RhysHuo/Fused_Vitis_Version.git
 cd Fused_Vitis_Version
 cp host.cpp ..
+cp host.h ..
 cd ..
 rm -rf Fused_Vitis_Version
 g++ -g -std=c++14 -I$XILINX_XRT/include -L${XILINX_XRT}/lib/ -I/mnt/scratch/rhyhuo/HLS_arbitrary_Precision_Types/include -o host.exe host.cpp host.h -lOpenCL -pthread -lrt -lstdc++
