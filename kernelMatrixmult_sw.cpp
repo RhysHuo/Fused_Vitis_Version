a96875e186f5a4723661437efa498e4fbccc867d
RhysHuo <RhysHuo_1120272267@163.com>
2022-07-19 16:23:38 +0100

    Update kernelMatrixmult_sw.cpp

--- kernelMatrixmult_sw.cpp
+++ kernelMatrixmult_sw.cpp
@@ -360,7 +360,8 @@ void mmult_wrapper_sw(ap_uint<2> mode, ap_int<32> *quantized_multiplier, ap_int<
 	//scale_sw(quantized_multiplier, shift, bias, zero_point_dst, clamp_max, clamp_min, N, M, P, C_fifo, B_index, B_index_loop, tail, write_fifo);
 	//printf("scale_sw completed \n");
 
-	writec_sw(N, P, write_fifo, C, array_c_adjust, B_index, B_index_loop, tail);
+	//writec_sw(N, P, write_fifo, C, array_c_adjust, B_index, B_index_loop, tail);
+	writec_sw(N, P, C_fifo, C, array_c_adjust, B_index, B_index_loop, tail);
 	printf("writec_sw completed \n");
 	
 }
