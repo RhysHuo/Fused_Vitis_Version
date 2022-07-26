9f426485ef46d659b5911528a01884895e7127ae
RhysHuo <RhysHuo_1120272267@163.com>
2022-07-26 15:28:18 +0100

    Delete control_host_debug.sh

--- control_host_debug.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/RhysHuo/Fused_Vitis_Version.git
-cd Fused_Vitis_Version
-cp host_debug.cpp ..
-cp host.h ..
-cd ..
-rm -rf Fused_Vitis_Version
-g++ -g -std=c++14 -I$XILINX_XRT/include -L${XILINX_XRT}/lib/ -I/mnt/scratch/rhyhuo/HLS_arbitrary_Precision_Types/include -o host_debug.exe host_debug.cpp host.h -lOpenCL -pthread -lrt -lstdc++
